c5cc0d9008ab4abb3055c7bcf10fc86dced49d62
AlinaZ <alinush0zahozi@gmail.com>
2023-04-13 12:33:10 +0300

    Update

--- sources/card.cpp
+++ sources/card.cpp
@@ -12,17 +12,19 @@ namespace ariel{
         this->card_value = val;
     }
 
+    //Get card type
     string Card::get_card_type(){
-        return card_type;
+        return this->card_type ;
     }
 
+    //Get card value
     string Card::get_card_value(){
-        return card_value;
+        return this->card_value;
     }
 
     // To string
     string Card::to_string(){
-        return "Card";
+        return "Card type is: " + this->card_type + " and Card value is: " + this->card_value;
     }
 
 }
