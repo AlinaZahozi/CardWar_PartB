dd7230d1ac0a36451e1c10d17a263dfe82366c47
AlinaZ <alinush0zahozi@gmail.com>
2023-04-16 18:14:51 +0300

    Almost Done

--- sources/game.cpp
+++ sources/game.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-//# include <unistd.h>
 #include <string>
 #include "game.hpp"
 #include "player.hpp"
@@ -33,73 +32,9 @@ Game::Game(Player& p1 ,Player& p2):
     }
 }
 
-/*
-// Copy constructor
-Game::Game(const Game& other):
-first_player(other.first_player),
-second_player(other.second_player),
-log(new string(*other.log)),
-last_turn(new string(*other.last_turn)),
-number_of_draws(other.number_of_draws){};
-
-
-// Copy assignment operator
-Game& Game::operator=(const Game& other) {
-    if(this != &other) {
-        first_player = other.first_player;
-        second_player = other.second_player;
-        *log = *other.log;
-        *last_turn = *other.last_turn;
-        number_of_draws = other.number_of_draws;
-    }
-    return *this;
-}
-
-// Move constructor
-Game::Game(Game&& other):
-    first_player(move(other.first_player)),
-    second_player(move(other.second_player)),
-    log(other.log),
-    last_turn(other.last_turn),
-    number_of_draws(other.number_of_draws){
-    other.log = nullptr;
-    other.last_turn = nullptr;
-}
-
-Game& Game::operator=(Game&& other) noexcept{
-    if(this != &other) {
-        // Release the resources held by the current object
-        delete log;
-        delete last_turn;
-        // Move the resources from the other object to the current object
-        first_player = other.first_player;
-        second_player = other.second_player;
-        //deck = std::move(other.deck);
-        //middle_cards = std::move(other.middle_cards);
-        //first_player_cards = std::move(other.first_player_cards);
-        //second_player_cards = std::move(other.second_player_cards);
-        number_of_draws = other.number_of_draws;
-        log = other.log;
-        last_turn = other.last_turn;
-
-        // Reset the other object
-        other.log = nullptr;
-        other.last_turn = nullptr;
-    }
-    return *this;
-}
-
-Game::~Game(){
-    delete this->log;
-    delete this->last_turn;
-}
-
-*/
-
 void Game::dividecards(){
     cout << to_string(cardsToDivide.cards.size()) << endl;
     for(int i = 52,j = 20; i > 0 ; i--){
-        //srand((unsigned) time(NULL));
         srand((unsigned)j);
         Card &c = cardsToDivide.cards.top();
         cardsToDivide.cards.pop();
@@ -108,16 +43,12 @@ void Game::dividecards(){
         cout << "rand:" << to_string(ran) << endl;
         if((ran%2) == 0 && first_player.stacksize() < 26){
             first_player.sPush(c);
-            //cout << "" << first_player.stacksize() << endl;
         }
         else if(second_player.stacksize() < 26){
             second_player.sPush(c);
-            //cout << "" << second_player.stacksize() << endl;
-            //cout << "" << second_player.stacksize() << endl;
         }
         else{
             first_player.sPush(c);
-            //cout << "" << first_player.stacksize() << endl;
         } 
     }
 }
@@ -134,6 +65,7 @@ Player Game::get_second_player(){
 int Game::get_number_of_draws(){
     return this->number_of_draws;
 }
+
 string Game::get_log(){
     return this->log;
 }
@@ -170,11 +102,8 @@ int Game::playTurn(string move){
     first_player.sPop();
     second_player.sPop();
     int strength1 = c1.get_strength();
-    //cout << "" << "strength1 = " << card_to_string(strength1) << endl;
     int strength2 = c2.get_strength();
-    //cout << "" << "strength2 = " << card_to_string(strength2) << endl;
     if(strength1 == 14 && strength2 == 2){
-        cout << "22222222222222222222" << endl;
         second_player.addWin();
         second_player.add_card_to_cards_won(card1);
         second_player.add_card_to_cards_won(card2);
@@ -184,7 +113,6 @@ int Game::playTurn(string move){
         return 2;
     }
     else if(strength1 == 2 && strength2 == 14){
-        cout << "3333333333333333" << endl;
         first_player.addWin();
         first_player.add_card_to_cards_won(card1);
         first_player.add_card_to_cards_won(card2);
@@ -194,7 +122,6 @@ int Game::playTurn(string move){
         return 1;
     }
     else{
-        cout << "44444444444444444" << endl;
         if(strength1 > strength2){
         first_player.addWin();
         first_player.add_card_to_cards_won(card1);
@@ -205,21 +132,15 @@ int Game::playTurn(string move){
         return 1;
         }
         else if(strength1 < strength2){
-        cout << "55555555555555555" << endl;
         second_player.addWin();
         second_player.add_card_to_cards_won(card1);
         second_player.add_card_to_cards_won(card2);
-        cout << "a" << endl;
         last_move.append(first_player.get_player_name() + " played " + c1.card_to_string() + ", " + second_player.get_player_name() + " played " + c2.card_to_string() + ". " + second_player.get_player_name() + " wins.\n");
-        cout << "b" << endl;
         appendToLastTurn(last_move);
-        cout << "c" << endl;
         appendTolog(last_move);
-        cout << "d" << endl;
         return 2;
         }
         else if(strength1 == strength2){
-            cout << "6666666666666666666" << endl;
             this->number_of_draws++;
             if(first_player.stacksize() == 0 ||second_player.stacksize() == 0){
                 printWiner();
@@ -228,26 +149,16 @@ int Game::playTurn(string move){
             else{
                 last_move.append(first_player.get_player_name() + " played " + c1.card_to_string() + ", " + second_player.get_player_name() + " played " + c2.card_to_string() + ". Draw." );
                 int winner = playTurn(last_move);
-                cout << "winner : "<< to_string(winner) << endl;
-                cout << "rfgrwe4gt" << endl;
                 if( winner == 1){
-                    cout << "here\n" ;
                     first_player.addWin();
-                    cout << " or here\n" ;
                     first_player.add_card_to_cards_won(card1);
                     first_player.add_card_to_cards_won(card2);
-                    cout << "7777777777777777" << endl;
                     return 1;
                 }
                 else if( winner == 2){
-                    cout << "here\n" ;
                     second_player.addWin();
-                    cout << " or here\n" ;
-                    cout << to_string(second_player.get_num_of_taken_cards()) << endl;
-                    cout << c1.card_to_string() << endl;
                     second_player.add_card_to_cards_won(card1);
                     second_player.add_card_to_cards_won(card2);
-                    cout << "8888888888888888" << endl;
                     return 2;
                 }
                 else return 0;
@@ -261,12 +172,11 @@ int Game::playTurn(string move){
 }
 
 void Game::printLastTurn(){
-    //cout << this->last_turn;
-    cout << "last_turn" << endl;
+    cout << this->last_turn;
 }
 
 void Game::playAll(){
-    //if(first_player.stacksize() == 0 ||second_player.stacksize() == 0) throw string("Game is already over");
+    if(first_player.stacksize() == 0 ||second_player.stacksize() == 0) throw string("Game is already over");
     bool flag = false;
     for (int i = 26; flag == false && i > 0 ;i--) {
         if(first_player.stacksize() == 0 ||second_player.stacksize() == 0){
@@ -317,4 +227,5 @@ void Game::appendToLastTurn(const string& appendString){
     this->last_turn.append(appendString);
 
 }
+
 }
